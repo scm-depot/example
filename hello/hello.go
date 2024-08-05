8467bec00ea79afa5bd9c95566d2ba4080d9c901
Ed Espino <espino@apache.org>
2024-08-05 00:23:44 -0700

    GREETING

--- hello/hello.go
+++ hello/hello.go
@@ -37,7 +37,7 @@ func usage() {
 }
 
 var (
-	greeting    = flag.String("g", "HELLO", "GREET with `greeting`")
+	greeting    = flag.String("g", "HELLO", "GREET with `GREETING`")
 	reverseFlag = flag.Bool("r", false, "Greet in reverse")
 )
 
